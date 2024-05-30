0aec1d0358e1b7bf17f00f7c3244bd054c37bfea
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-05-30 12:13:21 +0300

    did 20 lesson

--- script.js
+++ script.js
@@ -49,5 +49,7 @@
 // let character = 'Hello';
 // let count = 8;
 // let rows = ["Naomi", "Quincy", "CamperChan"];
-let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows[0]);
+// let rows = ["Naomi", "Quincy", "CamperChan"];
+// console.log(rows[0]);
+rows[2] = 10;
+console.log(rows)
