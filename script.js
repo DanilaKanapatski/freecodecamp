9b683e85f6b81e09316c852165ec4f3bb8485956
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-05-30 10:35:21 +0300

    did 5 lesson

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // let character;
 // let character = "Hello";
 // let character = 'Hello';
+// let character = 'Hello';
+// console.log(character);
 let character = 'Hello';
 console.log(character);
+character = "World"
