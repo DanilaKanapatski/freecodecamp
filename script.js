0485dd210c2352aaae73fe56fdf28cc7ea0395f2
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-05-31 16:24:37 +0300

    did 66 lesson

--- script.js
+++ script.js
@@ -199,6 +199,8 @@
 //     return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
 //   }
 
-function padRow(rowNumber, rowCount) {
-    return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
-  }
+// function padRow(rowNumber, rowCount) {
+//     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+//   }
+
+for (let i = 0; i < count; i += 1) {
