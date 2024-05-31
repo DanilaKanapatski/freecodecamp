390ea1194f605876d820c11880cd4e19d99e2df2
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-05-31 15:52:14 +0300

    did 62 lesson

--- script.js
+++ script.js
@@ -182,6 +182,11 @@
 //     return character.repeat(rowNumber)
 //   }
 
+// for (let i = 0; i < count; i = i + 1) {
+//     rows.push(padRow())
+//   }
+
 for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow())
+    rows.push(padRow(i+1, count))
+    console.log(i)
   }
