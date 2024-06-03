3e46f041ad426822c6de1acf04d20e371fdcfd32
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-03 16:22:08 +0300

    did 77 lesson

--- script.js
+++ script.js
@@ -232,6 +232,11 @@
 // if ("false") {
 //   console.log("Condition is true");
 // }
+// if ("") {
+//   console.log("Condition is true");
+// }
 if ("") {
   console.log("Condition is true");
+} else if (5 < 10) {
+  console.log("5 is less than 10")
 }
