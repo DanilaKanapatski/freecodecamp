f56426b7b95c6712b043c6a54167e468e8df8436
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-06 12:01:27 +0300

    did 104 lesson

--- script.js
+++ script.js
@@ -337,9 +337,9 @@
 // const unshifted = numbers.unshift(5)
 // console.log(numbers);
 // console.log(unshifted);
-const numbers = [1, 2, 3];
-const shifted = numbers.shift();
-console.log(shifted);
-const unshifted = numbers.unshift(5);
-console.log(unshifted);
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// const shifted = numbers.shift();
+// console.log(shifted);
+// const unshifted = numbers.unshift(5);
+// console.log(unshifted);
+// console.log(numbers);
