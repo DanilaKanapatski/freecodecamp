affaf02b84571deb6a84a89e51aa5f9b623969a5
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-06 12:13:59 +0300

    did 112 lesson

--- script.js
+++ script.js
@@ -363,10 +363,10 @@
     
 //   }
 // }
-for (let i = 1; i <= count; i++) {
-  if (inverted) {
-    rows.unshift(padRow(i, count));
-  } else {
-    rows.push(padRow(i,count));
-  }
-}
+// for (let i = 1; i <= count; i++) {
+//   if (inverted) {
+//     rows.unshift(padRow(i, count));
+//   } else {
+//     rows.push(padRow(i,count));
+//   }
+// }
