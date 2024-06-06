f7cb8213295f91bc41d9d4901d759ee4627364c9
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-06 15:00:57 +0300

    did 3 task

--- secondProject.js
+++ secondProject.js
@@ -12,18 +12,29 @@ console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
 
 function getGrade(score) {
     if (score == 100) {
-      return "A++"
+        return "A++"
     } else if (score >= 90) {
-      return "A"
+        return "A"
     } else if (score >= 80) {
-      return "B"
+        return "B"
     } else if (score >= 70) {
-      return "C"
+        return "C"
     } else if (score >= 60) {
-      return "D"
+        return "D"
     } else return "F"
-  }
-  
-  console.log(getGrade(96));
-  console.log(getGrade(82));
-  console.log(getGrade(56));
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
+
+function hasPassingGrade(score) {
+    if (getGrade(score) != "F") {
+        return true
+    } else return false
+}
+
+
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
