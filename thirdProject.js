bc7f2c44f37be3bea67e0208e3407ec3a39525c1
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-07 17:17:40 +0300

    did 132 task

--- thirdProject.js
+++ thirdProject.js
@@ -188,10 +188,12 @@ function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
 
-function defeatMonster () {
+function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
-    xp += monsters[fighting].level
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
 }
-function lose () {
-  
+function lose() {
+
 }
