82047b2a1a360b781debfe2e7c8fb2bbc09ed8c4
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-07 16:09:26 +0300

    did 117 task

--- thirdProject.js
+++ thirdProject.js
@@ -163,7 +163,8 @@ function fightDragon() {
 }
 
 function goFight() {
-    update(locations[3])
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
 }
 
 function attack () {
