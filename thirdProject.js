e83e856a38b1cd4b5b64a30b4af9b7f81c06b343
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-07 11:08:20 +0300

    did 46 task

--- thirdProject.js
+++ thirdProject.js
@@ -20,6 +20,9 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+function update(location){
+
+}
 function goTown() {
     button1.innerText = "Go to store";
     button2.innerText = "Go to cave";
