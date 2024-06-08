5b289baa5555a649d7713f687ba88ff2613941d5
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-08 20:27:04 +0300

    did 150 task

--- thirdProject.js
+++ thirdProject.js
@@ -193,6 +193,9 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
     health -= getMonsterAttackValue(monsters[fighting].level);
+    if (isMonsterHit()) {
+
+    }
     monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
