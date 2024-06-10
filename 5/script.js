005e7082fcb2e16f9dac1e6f351d2db9c29022cb
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-10 21:04:52 +0300

    did 63 task

--- 5/script.js
+++ 5/script.js
@@ -33,6 +33,8 @@ function getCaloriesFromInputs(list) {
     for(const item of list) {
         const currVal = cleanInputString(item.value);
         const invalidInputMatch = isInvalidInput(currVal);
-        if (invalidInputMatch) {}
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`)
+        }
     }
 }
