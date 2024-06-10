36fa1d51e0c045ccf8dd572b3bd7b76d5449186b
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-10 16:03:26 +0300

    did 48 task

--- 5/script.js
+++ 5/script.js
@@ -19,5 +19,6 @@ function isInvalidInput(str) {
 function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
-    let HTMLString = `<label for = "${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
+    let HTMLString = `<label for = "${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input type = "text" placeholder = "Name" id = "${entryDropdown.value}-${entryNumber}-name"></input>`;
 }  
