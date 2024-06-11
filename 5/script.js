dcc0d68bde647b54c23682b83fe51d9f2c087b40
Danila Kanapatski <danila_konopatskiy@mail.ru>
2024-06-11 10:34:19 +0300

    did 80 task

--- 5/script.js
+++ 5/script.js
@@ -48,6 +48,7 @@ function calculateCalories(e) {
     let consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
     const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+    output.innerHTML = ``
 }
 
 function getCaloriesFromInputs(list) {
